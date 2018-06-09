be8c7be538537e55d166d1294af51ee445f6d201
Raghu Betina <raghu@firstdraft.com>
2018-06-09 23:08:06 +0000

    Generate skiers resource with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :skiers
+  # Routes for the Skier resource:
+  # READ
+  get "/skiers", :controller => "skiers", :action => "index"
+  get "/skiers/:id", :controller => "skiers", :action => "show"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
