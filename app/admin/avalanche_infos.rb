920fdb0fb2dc3e7c60ea83724606fcf629effe11
Raghu Betina <raghu@firstdraft.com>
2018-06-09 23:08:40 +0000

    Whitelist columns in AvalancheInfo for ActiveAdmin

--- app/admin/avalanche_infos.rb
+++ app/admin/avalanche_infos.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register AvalancheInfo do
+
+ permit_params :location, :skier_id, :name, :details
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
